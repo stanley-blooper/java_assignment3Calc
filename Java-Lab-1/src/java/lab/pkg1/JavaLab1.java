e3c6966473bebdb209e0334c4669f45268c91bed
sjean <sjean@DESKTOP-UL2U2RM>
2021-02-11 13:06:02 -0500

    Even numbers from 0 to 100

---
+++ Java-Lab-1/src/java/lab/pkg1/JavaLab1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.lab.pkg1;
+
+/**
+ *
+ * @author sjean
+ */
+public class JavaLab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+	for (int num=0;num<101;num++){  //initiation and for loop
+            if (num%2==0)
+                System.out.println(num);
+           }
+    
+      
+    }
